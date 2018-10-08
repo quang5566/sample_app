31e4a43d2479ce83790d3585d13845324effda42
Quang Nguyen <nguyen.v.quang96@gmail.com>
2018-10-08 13:21:27 +0700

    Chapter 12: Password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_reader :remember_token, :activation_token
+  attr_reader :remember_token, :activation_token, :reset_token
   before_save :email_downcase
   before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: Settings.max_name}
@@ -35,9 +35,10 @@ class User < ApplicationRecord
     update remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false unless remember_digest
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
@@ -52,6 +53,19 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    @reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.exp_pass.hours.ago
+  end
+
   private
 
   def email_downcase
@@ -59,7 +73,7 @@ class User < ApplicationRecord
   end
 
   def create_activation_digest
-    @activation_token  = User.new_token
-    @activation_digest = User.digest(activation_token)
+    @activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
